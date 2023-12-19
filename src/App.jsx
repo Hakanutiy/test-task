90ff9a015be1951123bc1be278c2d2728dc74952
Haknutiy <thehaknutiy@gmail.com>
2023-12-19 14:09:04 +0600

    feat: построил архитектуру проекта

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import {BrowserRouter, Routes} from "react-router-dom";
+
+function App() {
+
+  return (
+    <div>
+    <BrowserRouter>
+    <Routes>
+
+    </Routes>
+    </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
